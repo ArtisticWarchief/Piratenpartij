c239fce663c271dc16eb05830f4ff08dbc1dca5e
ShaniMG <semg98@gmail.com>
2021-02-10 19:55:49 +0100

    implementatie van een methode om de waardes per attribuut van alle crewmates op te tellen en het begin van een methode om obstacles aan te maken

--- Piratenpartij/Ship.cs
+++ Piratenpartij/Ship.cs
@@ -8,26 +8,63 @@ namespace Piratenpartij
 {
     public class Ship
     {
-        public List<string> Crewmembers { get; set; }
-        public List<string> Cargo { get; set; }
+        public List<Crewmember> Crewmembers { get; set; }
+        public Dictionary<string, int> Cargo { get; set; }
         public string Destination { get; set; }
         public int Food { get; set; }
         public int Fun { get; set; }
         public string Status { get; set; }
         public int Money { get; set; }
+        public List<Obstacle> Obstacles { get; set; }
+
+        readonly Random random = new Random();
+
+        public Dictionary<string, int> AtttributesTotal()
+        {
+            int Intelligence = 0;
+            int Strength = 0;
+            int Hunger = 0;
+            int Trading = 0;
+
+            foreach (Crewmember Crew in Crewmembers) {
+                Intelligence += Crew.Attribute.GetAttributes()[0];
+                Strength += Crew.Attribute.GetAttributes()[1];
+                Hunger += Crew.Attribute.GetAttributes()[2];
+                Trading += Crew.Attribute.GetAttributes()[3];
+            }
+
+            Dictionary<string, int> CrewAtttributes = new Dictionary<string, int> {
+                { "intelligence", Intelligence },
+                { "strength", Strength },
+                { "hunger", Hunger },
+                { "trading", Trading }
+            };
+            //Console.WriteLine(CrewAtttributes);
+            return CrewAtttributes;
+        }
 
         public Ship(int crewAmount)
         {
-            //Crewmembers = new List<string>();
             for (int i = 0; i < crewAmount; i++) {
-                Crewmembers.Add(new Crewmember());
+                Crewmembers.Add(new Crewmember.Crewmember());
             }
+
+            int obstacleAmount = random.Next(10, 1000);
+
+            for (int i = 0; i < obstacleAmount; i++) {
+                Obstacles.Add(new Obstacle.Obstacle());
+            }
+
             Cargo = new Cargo();
             Destination = "";
             Food = 100;
             Fun = 100;
             Status = "Start";
             Money = 0;
+            //Console.WriteLine(AtttributesTotal().Count);
         }
-    }   
+    }
+
 }
+
+
