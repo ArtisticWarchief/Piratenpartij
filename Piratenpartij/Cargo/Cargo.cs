b4416ddd0300782b88178e069473bd2ef93a6e61
Verriken <python247@outlook.com>
2021-02-10 10:56:39 +0100

    First Cargo Harbor class

---
+++ Piratenpartij/Cargo/Cargo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piratenpartij.Cargo
+{
+    public class Cargo
+    {
+        public string Name { get; set; }
+        public int Price { get; set; }
+        public int LimitOnShip { get; set; }
+
+        public Cargo()
+        {
+
+        }
+    }
+}
---
+++ Piratenpartij/Harbor/Harbor.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Piratenpartij.Cargo;
+
+namespace Piratenpartij.Harbor
+{
+    class Harbor
+    {
+        private Ship ship;
+        readonly Random random = new Random();
+
+        public string Name { get; set; }
+        public int FoodPrice { get; set; }
+        public List<string> Destinations { get; set; }
+        public List<string> Crewmembers { get; set; }
+        public Dictionary<Cargo.Cargo, int> CargoInStock { get; set; }
+
+
+        public Harbor(Ship ship)
+        {
+            InitializeCargo();
+            InitializeCrewmembers();
+            this.ship = ship;
+            FoodPrice = random.Next(1, 10);
+        }
+
+        private void InitializeCargo()
+        {
+
+        }
+
+        private void InitializeCrewmembers()
+        {
+
+        }
+    }
+}
