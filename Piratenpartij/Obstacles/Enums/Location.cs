7d547f15a39c27d0705feb1e0291fdeb1ebb4cf1
samdadfar-atos <sam.dadfar@atos.net>
2021-02-11 11:44:57 +0100

    defined Trip & Location enum

---
+++ Piratenpartij/Obstacles/Enums/Location.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piratenpartij.Obstacles.Enums
+{
+    public enum Location
+    {
+        US,
+        EU,
+        AS,
+        ON_BOARD,
+    }
+}
---
+++ Piratenpartij/Obstacles/Enums/Trip.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piratenpartij.Obstacles.Enums
+{
+    public enum Trip
+    {
+        US_EU,
+        US_AS,
+        EU_AS,
+    }
+}
