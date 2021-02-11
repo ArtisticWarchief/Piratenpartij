47c4f0879b6e76cc8f00d4ec7f3ac546537fb42e
samdadfar-atos <sam.dadfar@atos.net>
2021-02-11 09:40:17 +0100

    testing making event

--- Piratenpartij/Program.cs
+++ Piratenpartij/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Piratenpartij.Obstacles;
 
 namespace Piratenpartij
 {
@@ -14,9 +15,18 @@ namespace Piratenpartij
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.EnableVisualStyles();
+            //Application.SetCompatibleTextRenderingDefault(false);
+            //Application.Run(new Form1());
+            //Event e1 = new Event(Event.ObstacleType.HANDELSCHIP);
+            //Console.WriteLine(e1.name);
+            List<Event> events = new List<Event>();
+            for (int i = 0; i < 100; i++)
+            {
+                Event e1 = EventMaker.GetRandomly();
+                events.Add(e1);
+            }
+            Console.WriteLine(events.Where(e => e.Difficulty <= 40).Count());
         }
     }
 }
