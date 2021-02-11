3184ef2228b9439a9af8e573c7e035c45e2fd106
Stan Berkhout <stan.berkhout@atos.net>
2021-02-11 10:20:28 +0100

    Changed Attributes to Abilitys

--- Piratenpartij/Attribute/AbilityNames.cs
+++ Piratenpartij/Attribute/AbilityNames.cs
@@ -4,10 +4,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Piratenpartij.Attribute
+namespace Piratenpartij.Abilities
 {
 
-    public enum AttributeNames
+    public enum AbilityNames
     {
         Intelligence,
         Strength,
--- Piratenpartij/Crewmember/Crewmember.cs
+++ Piratenpartij/Crewmember/Crewmember.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Piratenpartij.Abilities;
 
 namespace Crewmember
 {
@@ -11,7 +12,7 @@ namespace Crewmember
         private string[] groupNames = {"Patrice", "Douwe", "Ilona", "Isabel", "Shani", "Sam", "Jordi", "Eyse", "Ivanildo", "Jan", "Omar", "Stan"};
         public string Name { get; set; }
         public int Cost { get; set; }
-        public Attribute.Attribute Attribute { get; set; }
+        public Abilities Ability { get; set; }
 
         readonly Random random = new Random();
 
@@ -19,7 +20,7 @@ namespace Crewmember
         {
             Name = groupNames[random.Next(0, 11)];
             Cost = random.Next(10,1000);
-            Attribute = new Attribute.Attribute();
+            Ability = new Abilities();
         }
 
 
