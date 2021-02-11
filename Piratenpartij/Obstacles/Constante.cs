fc9e66943c4ce62177da703d616fcae29306ba35
samdadfar-atos <sam.dadfar@atos.net>
2021-02-11 10:35:52 +0100

    defined list of destinations

---
+++ Piratenpartij/Obstacles/Constante.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piratenpartij.Obstacles
+{
+    public class Constante
+    {
+        public List<KeyValuePair<Destination, int>> DESTINATIONS = new List<KeyValuePair<Destination, int>>() 
+        {
+            new KeyValuePair<Destination, int>(Destination.EU_AS, 10),
+            new KeyValuePair<Destination, int>(Destination.US_AS, 5),
+        };
+    }
+
+    public enum Destination
+    {
+        US_EU,
+        US_AS,
+        EU_AS,
+    }
+}
