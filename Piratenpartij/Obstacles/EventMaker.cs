2c30eb7c243cad46682e716d5a3f6cd32e84f268
samdadfar-atos <sam.dadfar@atos.net>
2021-02-12 09:23:14 +0100

    defined GetEvents for get list of randomly events base on main ship location and destination

--- Piratenpartij/Obstacles/EventMaker.cs
+++ Piratenpartij/Obstacles/EventMaker.cs
@@ -6,43 +6,63 @@ namespace Piratenpartij.Obstacles
 {
     class EventMaker
     {
-        //sort of events with their probability
-        readonly static List<KeyValuePair<EventType, int>> pairs = new List<KeyValuePair<EventType, int>>() {
-            new KeyValuePair<EventType, int>(EventType.ISLAND, 10),
-            new KeyValuePair<EventType, int>(EventType.NONE, 10),
-            new KeyValuePair<EventType, int>(EventType.HARBOR, 10),
-            new KeyValuePair<EventType, int>(EventType.MERCHANT_SHIP, 35),
-            new KeyValuePair<EventType, int>(EventType.PIRATES_SHIP, 35)
+        //All of the events with their predefined probability
+        readonly static Dictionary<EventType, int> EVENTS_PROBABILITY = new Dictionary<EventType, int>() {
+            { EventType.ISLAND, 10 },
+            { EventType.NONE, 10 },
+            { EventType.HARBOR, 10 },
+            { EventType.MERCHANT_SHIP, 35 },
+            { EventType.PIRATES_SHIP, 35 }
         };
-        private EventMaker() { }
-
-        public static EventMaker instance;
-
-        public static EventMaker GetInstance()
-        {
-            if(instance == null)
-            {
-                instance = new EventMaker();
-            }
-            return instance;
-        }
-
+       
         readonly static Random random = new Random();
+
+        //Get a random Event with 
         public static Event GetRandomly()
         {
-            EventType selectedEvent = EventType.NONE;
+            EventType selectedEvent = EventType.PIRATES_SHIP;
             int probability = 0;
             int randomValue = random.Next(1, 101);
-            for (int i = 0; i < pairs.Count; i++)
+            foreach(KeyValuePair<EventType, int> pair in EVENTS_PROBABILITY)
             {
-                probability += pairs[i].Value;
+                probability += pair.Value;
                 if (randomValue <= probability)
                 {
-                    selectedEvent = pairs[i].Key;
+                    selectedEvent = pair.Key;
                     break;
                 }
             }
             return new Event(selectedEvent);
         }
+
+        //Get list of randomly events depend on Ship's current location and destination
+        public static List<Event> GetEvents(Ship.Ship ship)
+        {
+            int countOfEvent = TripDifficulty.GetDifficulty(ship.CurrentLocation, ship.Destination);
+            List<Event> events = new List<Event>(countOfEvent);
+            Dictionary<EventType, int> counts = new Dictionary<EventType, int>();
+            foreach (KeyValuePair<EventType, int> pair in EVENTS_PROBABILITY)
+            {
+                int count = (int)Math.Round((double)(pair.Value * countOfEvent) / 100);
+                counts.Add(pair.Key, count);
+            }
+            if(counts.Sum<KeyValuePair<EventType, int>>(e => e.Value) < events.Capacity)
+            {
+                int last = counts.Last().Value;
+                counts.Remove(counts.Keys.Last());
+                counts.Add(EVENTS_PROBABILITY.Keys.Last(), ++last);
+            }
+            foreach (KeyValuePair<EventType, int> count in counts)
+            {
+                for (int i = 0; i < count.Value; i++)
+                {
+                    if (events.Count == events.Capacity) break;
+                    events.Add(new Event(count.Key));
+                }
+            }
+            Random rand = new Random();
+            return events.OrderBy<Event, int>(e => rand.Next()).ToList();
+        }
+
     }
 }
