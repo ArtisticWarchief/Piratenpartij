d6bd83dcff5f16b9c45a97ec2cf3f848bcbca7e6
Douwe <Douweroedema@live.nl>
2021-02-18 14:24:30 +0100

    !WIP islandObstacles

---
+++ Piratenpartij/Island.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piratenpartij
+{
+    class Island
+    {
+        private string[] islandNames = { "Hawaii", "Madagaskar", "Malediven", "Australië", "Palawan", "Santorini", "Maderira", "Gili" };
+
+        private string[] goodOptions = { "banana", "gold", "cargo", "pork" };
+        private string[] badOptions = { "disease", "lion attack", "earthquake" };
+
+        readonly Random random = new Random();
+
+        public string islandName { get; set; }
+
+        public IslandAttributes islandAttributes { get; set; }
+
+        public void GenerateIsland()
+        {
+            Console.WriteLine("test");
+        }
+    }
+}
---
+++ Piratenpartij/IslandAttributes.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piratenpartij
+{
+    class IslandAttributes
+    {
+
+        private string[] options = { "good", "bad" };
+        private string[] goodOptions = { "banana", "gold", "cargo", "pork" };
+        private string[] badOptions = { "disease", "lion attack", "earthquake" };
+
+        public string option { get; set; }
+
+
+        readonly Random random = new Random();
+
+        public string GenerateAttributes()
+        {
+            return option = options[random.Next(0, 1)];
+        }
+    }
+}
