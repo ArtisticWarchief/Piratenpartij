ce296b0e8c4e3a20d17b8d6238b62a72cd2c60e2
Douwe <Douweroedema@live.nl>
2021-02-26 13:05:27 +0100

    Island event adds cargo or removes a crewmember. Not complelely done, but it is something.

--- Piratenpartij/Obstacles/Events/IslandEvent.cs
+++ Piratenpartij/Obstacles/Events/IslandEvent.cs
@@ -4,43 +4,57 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Piratenpartij.Ship;
+using Piratenpartij.Cargos;
+using Crewmembers;
 
 namespace Piratenpartij.Obstacles
 {
     //IslandEvent could happen whenever an island appears along a trip
     public class IslandEvent : Event
     {
-        private string[] islandNames = { "Hawaii", "Madagaskar", "Malediven", "Australië", "Palawan", "Santorini", "Maderira", "Gili" };
-        private string[] goodOptions = { "Peugeot208", "AMSPortfolio", "Gold"};
-        private string[] badOptions = { "Disease", "Lion attack", "Earthquake" };
-
         readonly Random random = new Random();
-        public Ship.Ship ship;
+        public Ship.Ship ship = Ship.Ship.GetInstance();
 
         public string islandName { get; set; }
+        public List<Crewmember> Crewmembers { get; set; }
         public string option { get; set; }
+
+        private string[] islandNames = { "Hawaii", "Madagaskar", "Malediven", "Australië", "Palawan", "Santorini", "Maderira", "Gili" };
+        private string[] goodOptions = { "Peugeot208", "AMSPortfolio", "MountainHoliday" };
+        private string[] badOptions = { "Disease", "Lion attack", "Earthquake" };
+
         
         public IslandEvent() : base(EventType.ISLAND)
         {
             Boolean goodOption = random.Next(0, 2) > 0;
             islandName = islandNames[random.Next(0, 7)];
 
-            Console.WriteLine(goodOption + " " + islandName);
+            Dictionary<Cargo, int> cargoDict = ship.Cargo;
 
             if (goodOption) {
                 option = goodOptions[random.Next(0, 2)];
-                
-            }
-            else {
+                int goodOptionValue = random.Next(0, 5);
+                Cargo cargo;
+
+                if (option == "Peugeot208") {
+                     cargo = cargoDict.Keys.FirstOrDefault(e => e.GetType() == typeof(Peugeot208));
+                } else if(option == "AMSPortfolio") {
+                    cargo = cargoDict.Keys.FirstOrDefault(e => e.GetType() == typeof(AMSPortfolio));
+                } else {
+                    cargo = cargoDict.Keys.FirstOrDefault(e => e.GetType() == typeof(MountainHoliday));
+                }
+
+                ship.Cargo[cargo] += goodOptionValue;
+
+            } else {
                 option = badOptions[random.Next(0, 2)];
+
+                Crewmembers = ship.Crewmembers;
+
+                Crewmember randomCrewmember = Crewmembers[random.Next(0, Crewmembers.Count())];
+
+                ship.Crewmembers.Remove(randomCrewmember);
             }
-
-            setShipAttributes(goodOption, islandName, option);
-        }
-
-        public void setShipAttributes(Boolean goodOption, string islandName, string option)
-        {
-            
         }
     }
 }
