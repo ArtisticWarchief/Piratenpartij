2a4d807f613975ddf5be3b88d717417da91aae49
ShaniMG <39906764+ShaniMG@users.noreply.github.com>
2021-02-26 17:10:04 +0100

    Test UI voor het verkopen van Cargo en leeg UI bestand voor het testen van de Ship class

--- Piratenpartij/ShipTestUI.Designer.cs
+++ Piratenpartij/ShipTestUI.Designer.cs
@@ -3,12 +3,12 @@ namespace Piratenpartij
 {
     partial class ShipTestUI
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,28 +20,324 @@ namespace Piratenpartij
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
+            this.plusTop = new System.Windows.Forms.Button();
+            this.plusMiddle = new System.Windows.Forms.Button();
+            this.plusBottom = new System.Windows.Forms.Button();
+            this.plusDoubleTop = new System.Windows.Forms.Button();
+            this.plusDoubleMiddle = new System.Windows.Forms.Button();
+            this.plusDoubleBottom = new System.Windows.Forms.Button();
+            this.minusTop = new System.Windows.Forms.Button();
+            this.minusDoubleTop = new System.Windows.Forms.Button();
+            this.minusMiddle = new System.Windows.Forms.Button();
+            this.minusDoubleMiddle = new System.Windows.Forms.Button();
+            this.minusBottom = new System.Windows.Forms.Button();
+            this.minusDoubleBottom = new System.Windows.Forms.Button();
+            this.totalCargoTop = new System.Windows.Forms.TextBox();
+            this.sellCargo = new System.Windows.Forms.Button();
+            this.totalCargoMiddle = new System.Windows.Forms.TextBox();
+            this.totalCargoBottom = new System.Windows.Forms.TextBox();
+            this.totalValueSale = new System.Windows.Forms.TextBox();
+            this.cargoTypeTop = new System.Windows.Forms.TextBox();
+            this.cargoTypeMiddle = new System.Windows.Forms.TextBox();
+            this.cargoTypeBottom = new System.Windows.Forms.TextBox();
+            this.shipAmountCargoTopAmount = new System.Windows.Forms.TextBox();
+            this.shipAmountCargoMiddleAmount = new System.Windows.Forms.TextBox();
+            this.shipAmountCargoBottomAmount = new System.Windows.Forms.TextBox();
+            this.afterSaleMessage = new System.Windows.Forms.RichTextBox();
             this.SuspendLayout();
+            // 
+            // plusTop
+            // 
+            this.plusTop.Location = new System.Drawing.Point(466, 27);
+            this.plusTop.Name = "plusTop";
+            this.plusTop.Size = new System.Drawing.Size(25, 23);
+            this.plusTop.TabIndex = 0;
+            this.plusTop.Text = "+";
+            this.plusTop.UseVisualStyleBackColor = true;
+            this.plusTop.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // plusMiddle
+            // 
+            this.plusMiddle.Location = new System.Drawing.Point(466, 56);
+            this.plusMiddle.Name = "plusMiddle";
+            this.plusMiddle.Size = new System.Drawing.Size(25, 23);
+            this.plusMiddle.TabIndex = 1;
+            this.plusMiddle.Text = "+";
+            this.plusMiddle.UseVisualStyleBackColor = true;
+            this.plusMiddle.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // plusBottom
+            // 
+            this.plusBottom.Location = new System.Drawing.Point(466, 85);
+            this.plusBottom.Name = "plusBottom";
+            this.plusBottom.Size = new System.Drawing.Size(25, 23);
+            this.plusBottom.TabIndex = 2;
+            this.plusBottom.Text = "+";
+            this.plusBottom.UseVisualStyleBackColor = true;
+            this.plusBottom.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // plusDoubleTop
+            // 
+            this.plusDoubleTop.Location = new System.Drawing.Point(497, 27);
+            this.plusDoubleTop.Name = "plusDoubleTop";
+            this.plusDoubleTop.Size = new System.Drawing.Size(41, 23);
+            this.plusDoubleTop.TabIndex = 3;
+            this.plusDoubleTop.Text = "++";
+            this.plusDoubleTop.UseVisualStyleBackColor = true;
+            this.plusDoubleTop.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // plusDoubleMiddle
+            // 
+            this.plusDoubleMiddle.Location = new System.Drawing.Point(497, 56);
+            this.plusDoubleMiddle.Name = "plusDoubleMiddle";
+            this.plusDoubleMiddle.Size = new System.Drawing.Size(41, 23);
+            this.plusDoubleMiddle.TabIndex = 4;
+            this.plusDoubleMiddle.Text = "++";
+            this.plusDoubleMiddle.UseVisualStyleBackColor = true;
+            this.plusDoubleMiddle.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // plusDoubleBottom
+            // 
+            this.plusDoubleBottom.Location = new System.Drawing.Point(497, 85);
+            this.plusDoubleBottom.Name = "plusDoubleBottom";
+            this.plusDoubleBottom.Size = new System.Drawing.Size(41, 23);
+            this.plusDoubleBottom.TabIndex = 5;
+            this.plusDoubleBottom.Text = "++";
+            this.plusDoubleBottom.UseVisualStyleBackColor = true;
+            this.plusDoubleBottom.Click += new System.EventHandler(this.plus_Click);
+            // 
+            // minusTop
+            // 
+            this.minusTop.Location = new System.Drawing.Point(335, 27);
+            this.minusTop.Name = "minusTop";
+            this.minusTop.Size = new System.Drawing.Size(25, 23);
+            this.minusTop.TabIndex = 6;
+            this.minusTop.Text = "-";
+            this.minusTop.UseVisualStyleBackColor = true;
+            this.minusTop.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // minusDoubleTop
+            // 
+            this.minusDoubleTop.Location = new System.Drawing.Point(295, 27);
+            this.minusDoubleTop.Name = "minusDoubleTop";
+            this.minusDoubleTop.Size = new System.Drawing.Size(34, 23);
+            this.minusDoubleTop.TabIndex = 7;
+            this.minusDoubleTop.Text = "--";
+            this.minusDoubleTop.UseVisualStyleBackColor = true;
+            this.minusDoubleTop.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // minusMiddle
+            // 
+            this.minusMiddle.Location = new System.Drawing.Point(335, 56);
+            this.minusMiddle.Name = "minusMiddle";
+            this.minusMiddle.Size = new System.Drawing.Size(25, 23);
+            this.minusMiddle.TabIndex = 8;
+            this.minusMiddle.Text = "-";
+            this.minusMiddle.UseVisualStyleBackColor = true;
+            this.minusMiddle.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // minusDoubleMiddle
+            // 
+            this.minusDoubleMiddle.Location = new System.Drawing.Point(295, 56);
+            this.minusDoubleMiddle.Name = "minusDoubleMiddle";
+            this.minusDoubleMiddle.Size = new System.Drawing.Size(34, 23);
+            this.minusDoubleMiddle.TabIndex = 9;
+            this.minusDoubleMiddle.Text = "--";
+            this.minusDoubleMiddle.UseVisualStyleBackColor = true;
+            this.minusDoubleMiddle.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // minusBottom
+            // 
+            this.minusBottom.Location = new System.Drawing.Point(335, 85);
+            this.minusBottom.Name = "minusBottom";
+            this.minusBottom.Size = new System.Drawing.Size(25, 23);
+            this.minusBottom.TabIndex = 10;
+            this.minusBottom.Text = "--";
+            this.minusBottom.UseVisualStyleBackColor = true;
+            this.minusBottom.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // minusDoubleBottom
+            // 
+            this.minusDoubleBottom.Location = new System.Drawing.Point(295, 85);
+            this.minusDoubleBottom.Name = "minusDoubleBottom";
+            this.minusDoubleBottom.Size = new System.Drawing.Size(34, 23);
+            this.minusDoubleBottom.TabIndex = 11;
+            this.minusDoubleBottom.Text = "--";
+            this.minusDoubleBottom.UseVisualStyleBackColor = true;
+            this.minusDoubleBottom.Click += new System.EventHandler(this.minus_Click);
+            // 
+            // totalCargoTop
+            // 
+            this.totalCargoTop.Location = new System.Drawing.Point(394, 28);
+            this.totalCargoTop.Name = "totalCargoTop";
+            this.totalCargoTop.ReadOnly = true;
+            this.totalCargoTop.Size = new System.Drawing.Size(31, 22);
+            this.totalCargoTop.TabIndex = 12;
+            this.totalCargoTop.Text = "0";
+            // 
+            // sellCargo
+            // 
+            this.sellCargo.Location = new System.Drawing.Point(230, 179);
+            this.sellCargo.Name = "sellCargo";
+            this.sellCargo.Size = new System.Drawing.Size(75, 23);
+            this.sellCargo.TabIndex = 14;
+            this.sellCargo.Text = "SELL";
+            this.sellCargo.UseVisualStyleBackColor = true;
+            this.sellCargo.Click += new System.EventHandler(this.sellCargo_Click);
+            // 
+            // totalCargoMiddle
+            // 
+            this.totalCargoMiddle.Location = new System.Drawing.Point(394, 56);
+            this.totalCargoMiddle.Name = "totalCargoMiddle";
+            this.totalCargoMiddle.ReadOnly = true;
+            this.totalCargoMiddle.Size = new System.Drawing.Size(31, 22);
+            this.totalCargoMiddle.TabIndex = 15;
+            this.totalCargoMiddle.Text = "0";
+            // 
+            // totalCargoBottom
+            // 
+            this.totalCargoBottom.Location = new System.Drawing.Point(394, 85);
+            this.totalCargoBottom.Name = "totalCargoBottom";
+            this.totalCargoBottom.ReadOnly = true;
+            this.totalCargoBottom.Size = new System.Drawing.Size(31, 22);
+            this.totalCargoBottom.TabIndex = 16;
+            this.totalCargoBottom.Text = "0";
+            // 
+            // totalValueSale
+            // 
+            this.totalValueSale.Location = new System.Drawing.Point(331, 179);
+            this.totalValueSale.Name = "totalValueSale";
+            this.totalValueSale.ReadOnly = true;
+            this.totalValueSale.Size = new System.Drawing.Size(65, 22);
+            this.totalValueSale.TabIndex = 17;
+            this.totalValueSale.Text = "0";
+            // 
+            // cargoTypeTop
+            // 
+            this.cargoTypeTop.Location = new System.Drawing.Point(33, 28);
+            this.cargoTypeTop.Name = "cargoTypeTop";
+            this.cargoTypeTop.ReadOnly = true;
+            this.cargoTypeTop.Size = new System.Drawing.Size(153, 22);
+            this.cargoTypeTop.TabIndex = 18;
+            // 
+            // cargoTypeMiddle
+            // 
+            this.cargoTypeMiddle.Location = new System.Drawing.Point(33, 56);
+            this.cargoTypeMiddle.Name = "cargoTypeMiddle";
+            this.cargoTypeMiddle.ReadOnly = true;
+            this.cargoTypeMiddle.Size = new System.Drawing.Size(153, 22);
+            this.cargoTypeMiddle.TabIndex = 19;
+            // 
+            // cargoTypeBottom
+            // 
+            this.cargoTypeBottom.Location = new System.Drawing.Point(33, 84);
+            this.cargoTypeBottom.Name = "cargoTypeBottom";
+            this.cargoTypeBottom.ReadOnly = true;
+            this.cargoTypeBottom.Size = new System.Drawing.Size(153, 22);
+            this.cargoTypeBottom.TabIndex = 20;
+            // 
+            // shipAmountCargoTopAmount
+            // 
+            this.shipAmountCargoTopAmount.Location = new System.Drawing.Point(216, 28);
+            this.shipAmountCargoTopAmount.Name = "shipAmountCargoTopAmount";
+            this.shipAmountCargoTopAmount.ReadOnly = true;
+            this.shipAmountCargoTopAmount.Size = new System.Drawing.Size(52, 22);
+            this.shipAmountCargoTopAmount.TabIndex = 21;
+            // 
+            // shipAmountCargoMiddleAmount
+            // 
+            this.shipAmountCargoMiddleAmount.Location = new System.Drawing.Point(216, 57);
+            this.shipAmountCargoMiddleAmount.Name = "shipAmountCargoMiddleAmount";
+            this.shipAmountCargoMiddleAmount.ReadOnly = true;
+            this.shipAmountCargoMiddleAmount.Size = new System.Drawing.Size(52, 22);
+            this.shipAmountCargoMiddleAmount.TabIndex = 22;
+            // 
+            // shipAmountCargoBottomAmount
+            // 
+            this.shipAmountCargoBottomAmount.Location = new System.Drawing.Point(216, 86);
+            this.shipAmountCargoBottomAmount.Name = "shipAmountCargoBottomAmount";
+            this.shipAmountCargoBottomAmount.ReadOnly = true;
+            this.shipAmountCargoBottomAmount.Size = new System.Drawing.Size(52, 22);
+            this.shipAmountCargoBottomAmount.TabIndex = 23;
+            // 
+            // afterSaleMessage
+            // 
+            this.afterSaleMessage.Location = new System.Drawing.Point(59, 229);
+            this.afterSaleMessage.Name = "afterSaleMessage";
+            this.afterSaleMessage.Size = new System.Drawing.Size(432, 96);
+            this.afterSaleMessage.TabIndex = 24;
+            this.afterSaleMessage.Text = "";
             // 
             // ShipTestUI
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(598, 433);
+            this.Controls.Add(this.afterSaleMessage);
+            this.Controls.Add(this.shipAmountCargoBottomAmount);
+            this.Controls.Add(this.shipAmountCargoMiddleAmount);
+            this.Controls.Add(this.shipAmountCargoTopAmount);
+            this.Controls.Add(this.cargoTypeBottom);
+            this.Controls.Add(this.cargoTypeMiddle);
+            this.Controls.Add(this.cargoTypeTop);
+            this.Controls.Add(this.totalValueSale);
+            this.Controls.Add(this.totalCargoBottom);
+            this.Controls.Add(this.totalCargoMiddle);
+            this.Controls.Add(this.sellCargo);
+            this.Controls.Add(this.totalCargoTop);
+            this.Controls.Add(this.minusDoubleBottom);
+            this.Controls.Add(this.minusBottom);
+            this.Controls.Add(this.minusDoubleMiddle);
+            this.Controls.Add(this.minusMiddle);
+            this.Controls.Add(this.minusDoubleTop);
+            this.Controls.Add(this.minusTop);
+            this.Controls.Add(this.plusDoubleBottom);
+            this.Controls.Add(this.plusDoubleMiddle);
+            this.Controls.Add(this.plusDoubleTop);
+            this.Controls.Add(this.plusBottom);
+            this.Controls.Add(this.plusMiddle);
+            this.Controls.Add(this.plusTop);
             this.Name = "ShipTestUI";
-            this.Text = "ShipTestUI";
-            this.Load += new System.EventHandler(this.ShipTestUI_Load);
+            this.Load += new System.EventHandler(this.UserControl1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button plusTop;
+        private System.Windows.Forms.Button plusMiddle;
+        private System.Windows.Forms.Button plusBottom;
+        private System.Windows.Forms.Button plusDoubleTop;
+        private System.Windows.Forms.Button plusDoubleMiddle;
+        private System.Windows.Forms.Button plusDoubleBottom;
+        private System.Windows.Forms.Button minusTop;
+        private System.Windows.Forms.Button minusDoubleTop;
+        private System.Windows.Forms.Button minusMiddle;
+        private System.Windows.Forms.Button minusDoubleMiddle;
+        private System.Windows.Forms.Button minusBottom;
+        private System.Windows.Forms.Button minusDoubleBottom;
+        private System.Windows.Forms.TextBox totalCargoTop;
+        private System.Windows.Forms.Button sellCargo;
+        private System.Windows.Forms.TextBox totalCargoMiddle;
+        private System.Windows.Forms.TextBox totalCargoBottom;
+        private System.Windows.Forms.TextBox totalValueSale;
+        private System.Windows.Forms.TextBox cargoTypeTop;
+        private System.Windows.Forms.TextBox cargoTypeMiddle;
+        private System.Windows.Forms.TextBox cargoTypeBottom;
+        private System.Windows.Forms.TextBox shipAmountCargoTopAmount;
+        private System.Windows.Forms.TextBox shipAmountCargoMiddleAmount;
+        private System.Windows.Forms.TextBox shipAmountCargoBottomAmount;
+        private System.Windows.Forms.RichTextBox afterSaleMessage;
     }
 }
--- Piratenpartij/ShipTestUI.cs
+++ Piratenpartij/ShipTestUI.cs
@@ -7,19 +7,155 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Piratenpartij.Ships;
 
 namespace Piratenpartij
 {
     public partial class ShipTestUI : Form
     {
+        private Ship ship;
+
         public ShipTestUI()
         {
             InitializeComponent();
+            ship = Ship.GetInstance();
         }
 
-        private void ShipTestUI_Load(object sender, EventArgs e)
+        private void plus_Click(object sender, EventArgs e)
         {
-           
+            String whichButton = ((Button)sender).Name;
+            int newValue = 0;
+            int cargoValue = 100;
+            int newTotalValue = 0;
+            switch (whichButton) {
+                case "plusTop":
+                    Console.WriteLine("plusTop");
+                    newValue = int.Parse(totalCargoTop.Text) + 1;
+                    totalCargoTop.Text = newValue.ToString();
+                    break;
+                case "plusMiddle":
+                    Console.WriteLine("plusMiddle");
+                    newValue = int.Parse(totalCargoMiddle.Text) + 1;
+                    totalCargoMiddle.Text = newValue.ToString();
+                    break;
+                case "plusBottom":
+                    Console.WriteLine("plusBottom");
+                    newValue = int.Parse(totalCargoBottom.Text) + 1;
+                    totalCargoBottom.Text = newValue.ToString();
+                    break;
+                case "plusDoubleTop":
+                    Console.WriteLine("plusDoubleTop");
+                    newValue = int.Parse(totalCargoTop.Text) + 10;
+                    totalCargoTop.Text = newValue.ToString();
+                    break;
+                case "plusDoubleMiddle":
+                    Console.WriteLine("plusDoubleMiddle");
+                    newValue = int.Parse(totalCargoMiddle.Text) + 10;
+                    totalCargoMiddle.Text = newValue.ToString();
+                    break;
+                case "plusDoubleBottom":
+                    Console.WriteLine("plusDoubleBottom");
+                    newValue = int.Parse(totalCargoBottom.Text) + 10;
+                    totalCargoBottom.Text = newValue.ToString();
+                    break;
+                default:
+                    Console.WriteLine("No idea which button called plus_Click");
+                    break;
+            }
+            newTotalValue = (int.Parse(totalCargoTop.Text) + int.Parse(totalCargoMiddle.Text) + int.Parse(totalCargoBottom.Text)) * cargoValue;
+            totalValueSale.Text = newTotalValue.ToString();
+        }
+
+        private void minus_Click(object sender, EventArgs e)
+        {
+            String whichButton = ((Button)sender).Name;
+            int newValue1 = 0;
+            int cargoValue1 = 100;
+            int newTotalValue1 = 0;
+            switch (whichButton) {
+                case "minusTop":
+                    Console.WriteLine("minusTop");
+                    newValue1 = int.Parse(totalCargoTop.Text) - 1;
+                    if (newValue1 > -1) {
+                        totalCargoTop.Text = newValue1.ToString();
+                    }
+                    break;
+                case "minusMiddle":
+                    newValue1 = int.Parse(totalCargoMiddle.Text) - 1;
+                    if (newValue1 > -1) {
+                        totalCargoMiddle.Text = newValue1.ToString();
+                    }
+                    Console.WriteLine("minusMiddle");
+                    break;
+                case "minusBottom":
+                    newValue1 = int.Parse(totalCargoBottom.Text) - 1;
+                    if (newValue1 > -1) {
+                        totalCargoBottom.Text = newValue1.ToString();
+                    }
+                    Console.WriteLine("minusBottom");
+                    break;
+                case "minusDoubleTop":
+                    newValue1 = int.Parse(totalCargoTop.Text) - 10;
+                    if (newValue1 > -1) {
+                        totalCargoTop.Text = newValue1.ToString();
+                    }
+                    else {
+                        totalCargoTop.Text = "0";
+                    }
+                    Console.WriteLine("minusDoubleTop");
+                    break;
+                case "minusDoubleMiddle":
+                    newValue1 = int.Parse(totalCargoMiddle.Text) - 10;
+                    if (newValue1 > -1) {
+                        totalCargoMiddle.Text = newValue1.ToString();
+                    }
+                    else {
+                        totalCargoMiddle.Text = "0";
+                    }
+                    Console.WriteLine("minusDoubleMiddle");
+                    break;
+                case "minusDoubleBottom":
+                    newValue1 = int.Parse(totalCargoBottom.Text) - 10;
+                    if (newValue1 > -1) {
+                        totalCargoBottom.Text = newValue1.ToString();
+                    }
+                    else {
+                        totalCargoBottom.Text = "0";
+                    }
+                    Console.WriteLine("minusDoubleBottom");
+                    break;
+                default:
+                    Console.WriteLine("No idea which button called minus_Click");
+                    break;
+            }
+            newTotalValue1 = (int.Parse(totalCargoTop.Text) + int.Parse(totalCargoMiddle.Text) + int.Parse(totalCargoBottom.Text)) * cargoValue1;
+            totalValueSale.Text = newTotalValue1.ToString();
+
+        }
+        private void UserControl1_Load(object sender, EventArgs e)
+        {
+            cargoTypeTop.Text = ship.Cargo.ElementAt(0).Key.ToString().Replace("Piratenpartij.Cargos.","");
+            cargoTypeMiddle.Text = ship.Cargo.ElementAt(1).Key.ToString().Replace("Piratenpartij.Cargos.", "");
+            cargoTypeBottom.Text = ship.Cargo.ElementAt(2).Key.ToString().Replace("Piratenpartij.Cargos.", "");
+            shipAmountCargoTopAmount.Text = ship.Cargo.ElementAt(0).Value.ToString();
+            shipAmountCargoMiddleAmount.Text = ship.Cargo.ElementAt(1).Value.ToString();
+            shipAmountCargoBottomAmount.Text = ship.Cargo.ElementAt(2).Value.ToString();
+        }
+
+        private void sellCargo_Click(object sender, EventArgs e)
+        {
+            string afterSale = "You have sold: " + totalCargoTop.Text + " " + cargoTypeTop.Text + ", " + totalCargoMiddle.Text + " " + cargoTypeMiddle.Text + ", " + totalCargoBottom.Text + " " + cargoTypeBottom.Text + ". For a total of " + totalValueSale.Text + ".";
+            Console.WriteLine(afterSale);
+            shipAmountCargoTopAmount.Text = (int.Parse(shipAmountCargoTopAmount.Text) + int.Parse(totalCargoTop.Text)).ToString();
+            shipAmountCargoMiddleAmount.Text = (int.Parse(shipAmountCargoMiddleAmount.Text) + int.Parse(totalCargoMiddle.Text)).ToString();
+            shipAmountCargoBottomAmount.Text = (int.Parse(shipAmountCargoBottomAmount.Text) + int.Parse(totalCargoBottom.Text)).ToString();
+
+            totalCargoTop.Text = "0";
+            totalCargoMiddle.Text = "0";
+            totalCargoBottom.Text = "0";
+            totalValueSale.Text = "0";
+            afterSaleMessage.Text = afterSale;
+
         }
     }
 }
